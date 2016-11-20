c60fdea2747e0aefd6c2404a6a8e6e4686649b62
Misha Wagner <mishajw@gmail.com>
2016-11-20 11:01:48 +0000

    Refactor main.js

--- js/main.js
+++ js/main.js
@@ -1,46 +1,63 @@
 
-$(function() {
-  var WIDTH = 400,
-    HEIGHT = 300,
-    VIEW_ANGLE = 45,
-    ASPECT = WIDTH / HEIGHT,
-    NEAR = 0.1,
-    FAR = 10000;
-
+function Lights() {
+  
   var container = document.getElementById('main-container');
 
-  var renderer = new THREE.WebGLRenderer();
-  var camera = new THREE.PerspectiveCamera(
-    VIEW_ANGLE, ASPECT, NEAR, FAR);
+  var WIDTH = 400,
+    HEIGHT = 300;
+  
+  var renderer, camera, scene;
 
-  var scene = new THREE.Scene();
+  this.init = function() {
+    initCore();
+    initObjects();
+  }
 
-  scene.add(camera);
-  camera.position.z = 300;
+  function initCore() {
+    var VIEW_ANGLE = 45,
+      ASPECT = WIDTH / HEIGHT,
+      NEAR = 0.1,
+      FAR = 10000;
 
-  renderer.setSize(WIDTH, HEIGHT);
+    camera = new THREE.PerspectiveCamera(
+      VIEW_ANGLE, ASPECT, NEAR, FAR);
+    camera.position.z = 300;
 
-  container.appendChild(renderer.domElement);
+    scene = new THREE.Scene();
+    scene.add(camera);
 
-  var radius = 50,
-    segments = 16,
-    rings = 16;
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(WIDTH, HEIGHT);
 
-  var sphereMaterial = new THREE.MeshLambertMaterial(
-    { color: 0xCC0000 });
+    container.appendChild(renderer.domElement);
+  }
 
-  var sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(
-      radius, segments, rings),
-    sphereMaterial);
-  scene.add(sphere);
+  function initObjects() {
+    var radius = 50,
+      segments = 16,
+      rings = 16;
 
-  var pointLight = new THREE.PointLight(0xFFFFFF);
-  pointLight.position.x = 10;
-  pointLight.position.y = 50;
-  pointLight.position.z = 130;
-  scene.add(pointLight);
+    var sphereMaterial = new THREE.MeshLambertMaterial(
+      { color: 0xCC0000 });
 
-  renderer.render(scene, camera);
+    var sphere = new THREE.Mesh(
+      new THREE.SphereGeometry(
+        radius, segments, rings),
+      sphereMaterial);
+    scene.add(sphere);
+
+    var pointLight = new THREE.PointLight(0xFFFFFF);
+    pointLight.position.x = 10;
+    pointLight.position.y = 50;
+    pointLight.position.z = 130;
+    scene.add(pointLight);
+
+    renderer.render(scene, camera);
+  }
+}
+
+$(function() {
+  var lights = new Lights();
+  lights.init();
 });
 
